0b37ba9404f042716abcb7f0088190dd041c9c02
Egor <80205178+efedotof@users.noreply.github.com>
2024-08-29 20:39:06 +0300

    Delete lib directory

--- lib/intil.dart
+++
@@ -1,9 +0,0 @@
-
-
-
- import 'package:spisok_pokupok/widget/BeautifulContainer.dart';
-
-List<BeautifulContainer> keycontainerList = [];
-  List<String> keytitleStringList = [];
-  List<String> keyshoppingList = [];
-
